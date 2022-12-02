35eea233306522b0894f1c7d9e08b7261c449e14
Vinicius Carvalho <114104425+ViniciusCarvalhoDevy@users.noreply.github.com>
2022-12-02 09:05:53 -0300

    Add files via upload
    Problemas: No upload da imagens e na criação de sessões

--- blog/admin/header.php
+++ blog/admin/header.php
@@ -1,11 +1,56 @@
 <?php
 require 'config.php';
 
+session_start();
+    if($_SESSION['logado'] == true){
+        header('Location: view2.php');
+       
+    }
 
-$info = "sair";
-    header("Location: view2.php?sair=".$info);
+    if(isset($_GET['sair'])){
+        session_destroy();
+        header('Location: index.php');
+        die();
+    }
 
+$stmt = $conectar->prepare("SELECT * FROM posts ORDER BY id DESC");
 
+$stmt->execute();
+
+$results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+include_once('header.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <!--  slide 23-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/blog/CSS/view2.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <title>Posts-Views</title>
+</head>
+<body>
+      
+    <main >
+<div  class="d-flex">
+    <div class="conteiner-fluid">
+        
+           <div class="row">
+            <nav id="sidebarMenu" style="  height:316px" class="col-md-10  text-white bg-dark pt-5  ">
+                <h2>Bom dia,<?php echo $_SESSION['nome'];?>
+                </h2>
+                <p><a href="header.php">Deslogar</a></p>
+               
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a href="" class="nav-item">Controlar Posts</a></li>
+                    <li class="nav-item"><a href="">Controlar Slides</a></li>
+                </ul>
+            </nav>
 
--- blog/admin/logar.php
+++ blog/admin/logar.php
@@ -2,7 +2,7 @@
 require 'config.php';
 session_id();
 $logins = $_POST['logins'];
-$passwords = md5($_POST['passwords']);
+$passwords = $_POST['passwords'];
 
 $stmt = $conectar->prepare("SELECT * FROM users WHERE logins = :logins and passwords = :passwords");
 $stmt->bindParam(":logins",$logins);
@@ -10,13 +10,13 @@ $stmt->bindParam(":passwords",$passwords);
 $stmt->execute();
 
 if($stmt->rowCount() == 1){
-    $info = $stmt->fetch();
+    $info = $stmt->fetch(); 
     $_SESSION['logado'] = true;
     $_SESSION['id'] = $info['id'];
     $_SESSION['nome'] =$info['nome'];
     $_SESSION['logins'] = $info['logins'];
     $_SESSION['passwords'] = $info['passwords'];  
-    header("Location:view2.php?name=".$info['nome']);
+    header("Location:view2.php");
 }else{
     echo "Usurário Não Encontrado!";
 }
